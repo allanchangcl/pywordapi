d574f9bd538d28fbba43dfc9ab87f8df75cb9854
Allan Chang <allanchangcl@gmail.com>
2019-06-22 13:16:37 +0800

    Remove old themes

--- docs/conf.py
+++ docs/conf.py
@@ -5,49 +5,44 @@ import os
 
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.coverage',
-    'sphinx.ext.doctest',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
 ]
-if os.getenv('SPELLCHECK'):
-    extensions += 'sphinxcontrib.spelling',
+if os.getenv("SPELLCHECK"):
+    extensions += ("sphinxcontrib.spelling",)
     spelling_show_suggestions = True
-    spelling_lang = 'en_US'
+    spelling_lang = "en_US"
 
-source_suffix = '.rst'
-master_doc = 'index'
-project = 'Pywordapi'
-year = '2019'
-author = 'Allan Chang'
-copyright = '{0}, {1}'.format(year, author)
-version = release = '1.0.0'
+source_suffix = ".rst"
+master_doc = "index"
+project = "Pywordapi"
+year = "2019"
+author = "Allan Chang"
+copyright = "{0}, {1}".format(year, author)
+version = release = "1.0.0"
 
-pygments_style = 'trac'
-templates_path = ['.']
+pygments_style = "trac"
+templates_path = ["."]
 extlinks = {
-    'issue': ('https://github.com/clchangnet/pywordapi/issues/%s', '#'),
-    'pr': ('https://github.com/clchangnet/pywordapi/pull/%s', 'PR #'),
+    "issue": ("https://github.com/clchangnet/pywordapi/issues/%s", "#"),
+    "pr": ("https://github.com/clchangnet/pywordapi/pull/%s", "PR #"),
 }
-import sphinx_py3doc_enhanced_theme
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes",]
-html_theme_options = {
-    'githuburl': 'https://github.com/clchangnet/pywordapi/'
-}
+html_theme_path = ["_themes"]
+html_theme_options = {"githuburl": "https://github.com/clchangnet/pywordapi/"}
 
 html_use_smartypants = True
-html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = "%b %d, %Y"
 html_split_index = False
-html_sidebars = {
-   '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
-}
-html_short_title = '%s-%s' % (project, version)
+html_sidebars = {"**": ["searchbox.html", "globaltoc.html", "sourcelink.html"]}
+html_short_title = "%s-%s" % (project, version)
 
 napoleon_use_ivar = True
 napoleon_use_rtype = False
